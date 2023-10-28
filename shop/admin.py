90eb88b7af99065d8e1941ef4b32822043022ddb
Ali Rezaei <alirezaeim1380@gmail.com>
2023-10-28 23:29:42 +0330

    Add shop model admins

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from shop.models import Vendor, Order
+
+
+@admin.register(Vendor)
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ('id',)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id',)
